76ec3c1e874250903fd07731102f4d87ff966ad3
ycao6928 <yue.cao.au@gmail.com>
2021-03-03 15:24:34 +1100

    no message

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-#
-# for (thisfile in list.files("/albona/nobackup/yuec/benchmark_simulation/scSimBenchmarkData/inst/extdata")){
-#   print(thisfile)
-#   ExperimentHubData::makeExperimentHubMetadata("/albona/nobackup/yuec/benchmark_simulation/scSimBenchmarkData",fileName = thisfile)
-#
-# }
